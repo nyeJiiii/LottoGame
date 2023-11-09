6034e15bc879dbb72641c37e279ce5ba04f9ce6e
hjhan <hjhan.0621@gmail.com>
2023-11-10 08:36:57 +0900

    refactor: BonusNumber 클래스 변경사항 테스트코드에 적용

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -2,7 +2,9 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
-import lotto.errors.ErrorMessage;
+import java.util.Arrays;
+import java.util.List;
+import lotto.exception.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
@@ -11,13 +13,14 @@ import org.junit.jupiter.params.provider.ValueSource;
 class BonusNumberTest {
 
     BonusNumber bm = new BonusNumber();
+    List<Integer> luckyNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
 
     @ParameterizedTest
     @ValueSource(strings = {"k", "1.2"})
     @DisplayName("보너스 번호: 정수가 아닌 숫자가 들어올 때 예외처리")
     void test_setBonusNumber_1(String inputNumber) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> bm.setBonusNumber(inputNumber))
+                .isThrownBy(() -> bm.setBonusNumber(inputNumber, luckyNumbers))
                 .withMessage(ErrorMessage.WRONG_NUMBER_FORMAT.getMessage());
     }
 
@@ -26,16 +29,25 @@ class BonusNumberTest {
     @DisplayName("보너스 번호: Null 또는 Empty 값이 들어올 때 예외처리")
     void test_setBonusNumber_2(String inputNumber) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> bm.setBonusNumber(inputNumber))
+                .isThrownBy(() -> bm.setBonusNumber(inputNumber, luckyNumbers))
                 .withMessage(ErrorMessage.WRONG_NUMBER_FORMAT.getMessage());
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"0", "46"})
-    @DisplayName("보너스 번호: 1~45 범위를 벗어나는 숫자가 있을 때 예외처리")
+    @DisplayName("보너스 번호: 1~45 범위를 벗어나는 숫자일 때 예외처리")
     void test_setBonusNumber_3(String inputNumber) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> bm.setBonusNumber(inputNumber))
+                .isThrownBy(() -> bm.setBonusNumber(inputNumber, luckyNumbers))
                 .withMessage(ErrorMessage.WRONG_NUMBER_RANGE.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "3"})
+    @DisplayName("보너스 번호: 당첨 번호와 중복되는 숫자일 때 예외처리")
+    void test_setBonusNumber_4(String inputNumber) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> bm.setBonusNumber(inputNumber, luckyNumbers))
+                .withMessage(ErrorMessage.WRONG_BONUS_NUMBER.getMessage());
     }
 }
