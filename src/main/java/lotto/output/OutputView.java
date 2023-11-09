95cfa478a12df3017f060d62d1daaed2cf6a1914
hjhan <hjhan.0621@gmail.com>
2023-11-10 03:03:35 +0900

    refactor: 총 수익률 출력하는 내용 OutputView에 추가

--- src/main/java/lotto/output/OutputView.java
+++ src/main/java/lotto/output/OutputView.java
@@ -5,7 +5,8 @@ public enum OutputView {
     NUMBER_OF_LOTTOS("개를 구매했습니다."),
     INPUT_LUCKY_NUMBER("\n당첨 번호를 입력해 주세요."),
     INPUT_BONUS_NUMBER("\n보너스 번호를 입력해 주세요."),
-    STATISTICS("\n당첨 통계\n" + "---");
+    STATISTICS("\n당첨 통계\n" + "---"),
+    TOTAL_PROFIT_RATIO("총 수익률은 %.1f%%입니다.");
 
     private final String message;
 
